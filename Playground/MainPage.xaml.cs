9aae9630ec4f14d7b369645dc411049afad1de8a
Harry <himion0@gmail.com>
2017-10-12 23:59:18 +0100

    Added MainPage

---
+++ Playground/MainPage.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+using System.Diagnostics;
+using Playground.Helpers;
+
+namespace Playground.Pages {
+
+    /// <summary>
+    /// This is the Main Page to help navigate to the other demonstrations
+    /// </summary>
+    public partial class MainPage : ContentPage {
+
+        // The Pages
+        public List<Page> Pages { get; }
+
+        public MainPage() {
+            // Rememeber to add your new page in here so that it shows up in the
+            // list
+            Pages = new List<Page>{
+                new PlaygroundPage(),
+                new RealmPage(),
+                new TaskAroundPage()
+            };
+            BindingContext = this;
+            InitializeComponent();
+        }
+
+        // Navigate to the new page, or explain why it failed!
+        void Handle_ItemTapped(object sender, ItemTappedEventArgs args) {
+            var page = args.Item as Page;
+            if (page == null) {
+                DisplayAlert("Error", "Invalid Page", "Ok");
+                return;
+            }
+            try {
+                Navigation.PushAsync(page);
+            } catch (Exception e) {
+                Debug.WriteLine(e);
+                DisplayAlert("Failed to Open page", e.Message, "ok");
+            }
+        }
+    }
+}
