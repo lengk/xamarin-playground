6ecf3561a4ca87b4397d5c1b71bf6f7899d1fd91
Harry <himion0@gmail.com>
2018-01-23 09:57:07 +0000

    Compiling Xaml

--- Playground/App.xaml.cs
+++ Playground/App.xaml.cs
@@ -3,13 +3,14 @@ using Playground.Pages;
 using Playground.Helpers;
 using System;
 using System.Reflection;
+using Xamarin.Forms.Xaml;
 
+[assembly: XamlCompilation(XamlCompilationOptions.Compile)]
 namespace Playground {
     public partial class App : Application {
         public App() {
             InitializeComponent();
-            MainPage = new MultiColPage();
-            //MainPage = new NavigationPage(new MainPage());
+            MainPage = new NavigationPage(new MainPage());
         }
 
         protected override void OnStart() {
