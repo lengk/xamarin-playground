ce71ab2de567ff0ffa4b057fd75a71072f83a687
Harry <himion0@gmail.com>
2018-01-23 09:58:12 +0000

    Disabled button playground page

---
+++ Playground/Pages/DisabledButtonPage.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using System.Diagnostics;
+using System.Runtime.CompilerServices;
+
+namespace Playground.Pages {
+    public partial class DisabledButtonPage : ContentPage {
+        public DisabledButtonPage() {
+            InitializeComponent();
+        }
+
+
+        bool buttondown = false;
+        async void Handle_Clicked(object sender, EventArgs args) {
+            if (buttondown) {
+                Debug.WriteLine("Button Pressed while Pressing!");
+            } else {
+                Debug.WriteLine("Button Dislabed");
+            }
+            SetButtonEnabled(false);
+            int delay;
+            int.TryParse(EntryDelay.Text, out delay);
+            buttondown = true;
+            await Task.Delay(delay == 0 ? 1000 : delay);
+            buttondown = false;
+            SetButtonEnabled(true);
+            Debug.WriteLine("Button Enabled");
+        }
+
+        void SetButtonEnabled(bool val) {
+            Button.IsEnabled = val;
+        }
+
+        async void LockClicked(object sender, EventArgs args) {
+            int delay;
+            int.TryParse(EntryDelay.Text, out delay);
+            delay = delay == 0 ? 1000 : delay;
+            Debug.WriteLine($"Delaying by {delay}");
+            await Task.Delay(delay);
+        }
+    }
+
+}
