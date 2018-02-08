356bb4ac5a8764d79410628e586e0c79fb8d6cfb
Harry <himion0@gmail.com>
2018-02-08 23:25:18 +0000

    Demostrating popup

---
+++ Playground/Extensions/ContentPageExtensions.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Windows.Input;
+using Xamarin.Forms;
+namespace Playground.Extensions
+{
+    public static class ContentPageExtensions
+    {
+
+        public static void ShowPopupOver(this ContentPage page, View v, string text, int padding = 5, short xpos = 1, short ypos = 1)
+        {
+            v.IsEnabled = false;
+            Frame popupframe = new Frame();
+            popupframe.Content = new Label
+            {
+                Text = text,
+                TextColor = Color.White,
+                FontSize= 15
+            };
+            popupframe.BackgroundColor = Color.Transparent;
+
+            var content = page.Content;
+
+            if (content is Layout<View>)
+            {
+
+                var layout = content as Layout<View>;
+                var contentview = new ContentView();
+
+                ICommand oldcommand = null;
+                var newcommand = new Command(() =>
+                {
+                    layout.Children.Remove(contentview);
+                    layout.Children.Remove(popupframe);
+                    layout.ForceLayout();
+                    if (oldcommand != null)
+                    {   
+                        oldcommand.Execute(null);
+                    }
+                    if (v is Button)
+                    {
+                        ((Button)v).SendClicked();
+                    }
+                });
+
+                foreach (var GR in v.GestureRecognizers)
+                {
+                    if (GR is TapGestureRecognizer)
+                    {
+                        oldcommand = ((TapGestureRecognizer)GR).Command;
+                        ((TapGestureRecognizer)GR).Command = newcommand;
+                        break;
+                    }
+                }
+
+                if (v is Button)
+                {
+                    oldcommand = new Command(() =>
+                    {
+                        ((Button)v).SendClicked();
+                    });
+                }
+
+                v.IsEnabled = true;
+
+                contentview.WidthRequest = page.Width;
+                contentview.HeightRequest = page.Height;
+                contentview.TranslationX = page.X;
+                contentview.TranslationY = page.Y;
+                contentview.BackgroundColor = Color.Black;
+                contentview.Opacity = 0.8;
+
+
+                layout.Children.Add(contentview);
+                layout.RaiseChild(v);
+                layout.Children.Add(popupframe);
+
+                popupframe.GestureRecognizers.Add(new TapGestureRecognizer()
+                {
+                    Command = newcommand
+                });
+
+                popupframe.TranslationX = v.Width + v.X + padding;
+                popupframe.TranslationY = v.Height + v.Y + padding;
+            }
+
+            if (!v.IsEnabled)
+            {
+                v.IsEnabled = true;
+            }
+        }
+    }
+}
--- Playground/Pages/OverlayView.xaml.cs
+++ Playground/Pages/OverlayView.xaml.cs
@@ -1,15 +1,13 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Threading.Tasks;
+using Playground.Extensions;
 using Xamarin.Forms;
-using Xamarin.Forms.PlatformConfiguration;
 
 namespace Playground.Pages
 {
     public partial class OverlayView : ContentPage
     {
         Frame f = new Frame();
-            
+
         public OverlayView()
         {
             InitializeComponent();
@@ -18,32 +16,31 @@ namespace Playground.Pages
         protected override void OnAppearing()
         {
             base.OnAppearing();
+            this.ShowPopupOver(button, "asda");
         }
 
         void Handle_Clicked(object sender, System.EventArgs e)
         {
-            ShowPopupOver(button,"asda");
+            
         }
 
-        void ShowPopupOver(View v, string text, int padding = 5){
-            Frame f = new Frame();
-            f.Content = new Label
+        ContentPage GetContentPage(Page page)
+        {
+            if (page is ContentPage)
             {
-                Text = text
-            };
-            f.BackgroundColor = Color.Accent;
-            f.Opacity = 0.2;
-            f.TranslationX = v.Width + v.X + padding;
-            f.TranslationY = v.Height + v.Y + padding;
-            if (Application.Current.MainPage is Page)
-            {
-                if (Content is Layout<View>)
-                {
-
-                    var layout = Content as Layout<View>;
-                    layout.Children.Add(f);
-                }
+                return (ContentPage)page;
             }
+
+            if (page is MultiPage<Page>)
+            {
+                return GetContentPage(((MultiPage<Page>)page).CurrentPage);
+            }
+            if (page is NavigationPage)
+            {
+                return GetContentPage((page as NavigationPage).CurrentPage);
+            }
+            throw new ArgumentException("Cannot find page in view stack");
         }
+
     }
 }
